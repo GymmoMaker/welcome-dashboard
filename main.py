f0aab5c4cf1067361a91c24c15ff3a7d06966ae3
GymmoMaker <greatgymmo@gmail.com>
2025-08-12 23:45:42 +0200

    Added temperature

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-from datetime import date
 from babel.dates import format_date
 import datetime
 import python_weather
 import asyncio
-import os
 
-async def main() -> None:
-    today = datetime.date.today()
-    print("De datum is " + format_date(today, format = 'long', locale='nl'))
-    print("Het weeknummer is " + today.strftime("%V"))
+LABEL_WIDTH = 20
+VALUE_WIDTH = 10
+LOCATION = 'Katwijk'
 
+def get_current_date(date):
+    return format_date(date, format = 'long', locale='nl')
+
+def get_week_number(date):
+    return date.strftime("%V")
+
+async def get_temperature():
     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
-        weather = await client.get('Leiden')
-        print("De temperatuur is " + str(weather.temperature) + " graden")
+        return (await client.get(LOCATION)).temperature
+
+def f_to_c(degrees):
+    #Convert Fahrenheit to Celsius
+    return (degrees - 32) / 1.8
+
+async def main():
+    today = datetime.date.today()
+    temp = await get_temperature()
+
+    print(f"{'Datum:':{LABEL_WIDTH}} {get_current_date(today):<{VALUE_WIDTH}}")
+    print(f"{'Weeknummer:':{LABEL_WIDTH}} {get_week_number(today):<{VALUE_WIDTH}}")
+    print(f"{'Temperatuur(F):':{LABEL_WIDTH}} {round(temp):<{VALUE_WIDTH}}")
+    print(f"{'Temperatuur(C):':{LABEL_WIDTH}} {round(f_to_c(temp)):<{VALUE_WIDTH}}")
 
 if __name__ == "__main__":
     asyncio.run(main())
